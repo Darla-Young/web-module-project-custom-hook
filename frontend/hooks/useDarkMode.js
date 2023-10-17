1d3fc8b61e0892b7f0b2514415f67fd7667fe6ba
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-10-17 12:26:25 -0500

    added missing semicolons

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -5,8 +5,8 @@
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = (initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage("darkMode", initialValue)
-    return [darkMode, setDarkMode]
+    const [darkMode, setDarkMode] = useLocalStorage("darkMode", initialValue);
+    return [darkMode, setDarkMode];
 };
 
-export default useDarkMode
+export default useDarkMode;
--- frontend/hooks/useLocalStorage.js
+++ frontend/hooks/useLocalStorage.js
@@ -1,22 +1,22 @@
 // * [?] Build a hook called (nobody knows) that takes in a key value and an initialValue.
 // * [?] Use the code from GP to build the the hook. (wtf is GP?)
-import { useState } from 'react'
+import { useState } from 'react';
 
 const useLocalStorage = (key, initialValue) => {
     const [state, setState] = useState(() => {
         if (localStorage.getItem(key)) {
-            return JSON.parse(localStorage.getItem(key))
+            return JSON.parse(localStorage.getItem(key));
         }
-        localStorage.setItem(key, JSON.stringify(initialValue))
-        return initialValue
-    })
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+    });
 
     const setStoredState = (value) => {
-        localStorage.setItem(key, JSON.stringify(value))
-        setState(value)
-    }
+        localStorage.setItem(key, JSON.stringify(value));
+        setState(value);
+    };
 
-    return([state, setStoredState])
+    return([state, setStoredState]);
 };
 
-export default useLocalStorage
+export default useLocalStorage;
